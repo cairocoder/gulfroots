fd310b95757beaeb679740cebfa408e7ba07b0b4
bassemehab <bassemehab94@gmail.com>
2017-12-18 19:40:39 +0200

    seeds bug solved

--- database/seeds/FiltersTableSeeder.php
+++ database/seeds/FiltersTableSeeder.php
@@ -22,7 +22,7 @@ class FiltersTableSeeder extends Seeder
         DB::table('filters')->insert(['name' => 'اعلانات مدفوعه مميزة', 'type' => 'isinMain', 'group_id' => '2']);
         DB::table('filters')->insert(['name' => 'اعلانات مدفوعه عاجلة', 'type' => 'isBreaking', 'group_id' => '2']);
         DB::table('filters')->insert(['name' => 'اعلانات مدفوعه ملونة', 'type' => 'isColored', 'group_id' => '2']);
-        DB::table('filters')->insert(['name' => 'أفضل الاعلانات', 'isinTop' => '', 'group_id' => '2']);
+        DB::table('filters')->insert(['name' => 'أفضل الاعلانات', 'type' => 'isinTop', 'group_id' => '2']);
 
         DB::table('filters')->insert(['name' => 'عرض', 'group_id' => '3']);
         DB::table('filters')->insert(['name' => 'طلب', 'group_id' => '3']);
